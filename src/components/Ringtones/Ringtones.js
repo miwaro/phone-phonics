ff7643d33b2d1d09c09e4811e15a26c1a5fc64ff
Michael <mikerooze12@gmail.com>
2020-10-21 04:30:27 -0700

    Add nature images and styles

--- src/components/Ringtones/Ringtones.js
+++ src/components/Ringtones/Ringtones.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import { NavLink } from "react-router-dom";
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 const ringtonesJSON = require('../../data/ringtones.json');
 
@@ -10,21 +11,29 @@ const RingtonePage = () => {
         const audio = new Audio(audioFilePath);
         audio.play();
     }
-    
+
+   
+
     return (
-        <div>
-            <div className="ringtones">
+        <div className="main-container">
+            <div className="ringtone-grid">
                 {ringtonesJSON.map(ringtone => (
-                    <div key={ringtone.id} className="ringtone">
+                    <div key={ringtone.id} className="ringtone-container">
+                        <img src={ringtone.img} alt="nature"/>
                         <Button
-                            size="small"
-                            variant="contained"
-                            color='primary'
-                            onClick={() => playRingtone(ringtone.path)}
+                            style={{position: 'absolute', top: 10, left: 10, color: '#FFF'}}
+                            size="large"
+                            variant="outlined"
                         >
                             {ringtone.title}
                         </Button>
-                        <NavLink to={ringtone.path} target="_blank" download>Download</NavLink>
+                        <button className="play-button" onClick={() => playRingtone(ringtone.path)}>
+                            <PlayArrowIcon 
+                                fontSize="large"
+                            />
+                        </button>
+                        <NavLink className="download-link" to={ringtone.path} target="_blank" download>Download Ringtone</NavLink>
+    
                     </div>
                 ))}
 
