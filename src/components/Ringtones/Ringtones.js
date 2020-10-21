a36c34f9a9adda7831778b4ab6dd0d39786c1625
Michael <mikerooze12@gmail.com>
2020-10-20 23:57:54 -0700

    Map JSON data to ringtones

--- src/components/Ringtones/Ringtones.js
+++ src/components/Ringtones/Ringtones.js
@@ -1,51 +1,46 @@
-import React, { useState } from 'react';
-import randomize from '../../audioclips/randomize.wav';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 
 const ringtonesJSON = require('../../data/ringtones.json');
 
-
 const RingtonePage = () => {
 
-    const [ringtones, setRingtones] = useState(ringtonesJSON);
-
-    const playRingtone = () => {
-        playSound(randomizeAudio);
+    const playRingtone = audioFilePath => {
+        const audio = new Audio(audioFilePath);
+        audio.play();
     }
 
-    const randomizeAudio = new Audio(ringtones.path);
-
-    const playSound = audioFile => {audioFile.play()};
-
+    const downloadRingtone = downloadFile => {
+        const download = new Audio(downloadFile);
+        download.play();
+    }
     
-
-
-    // function playRingtone() {
-
-    //     setRingtones(prevRingtone => prevRingtone.path)
-    // }
-
-
     return (
         <div>
-
             <div className="ringtones">
-                {ringtones.map(ringtone => (
+                {ringtonesJSON.map(ringtone => (
                     <div key={ringtone.id} className="ringtone">
                         <Button
                             size="small"
                             variant="contained"
                             color='primary'
-                            onClick={playRingtone}>
+                            onClick={() => playRingtone(ringtone.path)}
+                        >
                             {ringtone.title}
-                            {/* {ringtone.fileName} */}
+                        </Button>
+                        <Button
+                            size="small"
+                            variant="contained"
+                            color='secondary'
+                            onClick={() => playRingtone(ringtone.download)}
+                        >
+                            Download Ringtone
                         </Button>
                     </div>
                 ))}
 
             </div>
         </div>
-
     )
 }
 
